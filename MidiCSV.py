fff6f04c220021743198b6472c14fde1514d82ba
Xander <acox9567@bths.edu>
2019-06-13 10:29:44 -0400

    i dont even know man

--- MidiCSV.py
+++ MidiCSV.py
@@ -1,28 +1,18 @@
 import py_midicsv
 import os
 
+open("Midi Data/ptb.train.txt", "w").close
+file = open("Midi Data/ptb.train.txt", "a")
 
-def a_bach_keys():
-    for filename in os.listdir("Midi Data/Bach/keyboard"):
-        csv_arr = py_midicsv.midi_to_csv("Midi Data/Bach/keyboard/" + filename)
+for filename in os.listdir("Midi Data/Bach/sonatas/cello/"):
+        csv_arr = py_midicsv.midi_to_csv("Midi Data/Bach/sonatas/cello/" + filename)
         csv_str = ""
 
         for i in range(0, csv_arr.__len__()):
             csv_str += (csv_arr[i])
 
         csv_str += "\n\n"
-
-        file = open("Midi Data/MidiCsvData.txt", "a")
+        file = open("Midi Data/ptb.train.txt", "a")
         file.write(csv_str)
-    return
-
-
-def initialize():
-    open("Midi Data/MidiCsvData.txt", "w").close
-    a_bach_keys()
-    return
-
-
-initialize()
 
 print("\nDone")
